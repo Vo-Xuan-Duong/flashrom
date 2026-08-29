f46c776aa92aef4de8a0a410d8e36dfaa4b92cd6
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 01:44:01 +0700

    feat: expose recovery and wipe commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,7 +6,9 @@ pub fn run() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![
             android::detect_device,
-            android::reboot_device
+            android::reboot_device,
+            android::boot_twrp,
+            android::factory_reset
         ])
         .run(tauri::generate_context!())
         .expect("error while running FlashROM");
