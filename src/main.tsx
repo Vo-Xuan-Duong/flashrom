d43ce3c2cfe01900aee7c37a37bc2030c4e0ccfc
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 02:08:00 +0700

    style: load guarded flash controls

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import "./rom.css";
 import "./plan.css";
 import "./sideload.css";
+import "./flash.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
