8b6d4600ce1540546f4eb0b4bc0dd977b64df707
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 01:54:02 +0700

    style: load flash plan styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./rom.css";
+import "./plan.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
