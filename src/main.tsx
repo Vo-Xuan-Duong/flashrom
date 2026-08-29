a1e86b11ba90ddce39e9a03e8d72f810aed0f935
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 02:05:00 +0700

    style: load ADB sideload styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./rom.css";
 import "./plan.css";
+import "./sideload.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
