6ec432e74761156a94cce4a9c332dcf534fcda83
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-30 20:01:27 +0700

    style: load executor styles

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./final-plan.css";
 import "./guard.css";
 import "./restore.css";
 import "./restore-profile.css";
+import "./executor.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
