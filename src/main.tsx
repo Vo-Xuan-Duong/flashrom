35854753dff106d4707c9dff62d1a77bc6144e6f
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-31 09:42:49 +0700

    feat: mount ROM archive inspector

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import RecoveryCenter from "./components/RecoveryCenter";
+import RomArchivePanel from "./components/RomArchivePanel";
 import "./styles.css";
 import "./rom.css";
 import "./plan.css";
@@ -13,10 +14,12 @@ import "./restore.css";
 import "./restore-profile.css";
 import "./executor.css";
 import "./recovery-center.css";
+import "./archive-panel.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <RomArchivePanel />
     <RecoveryCenter />
   </StrictMode>,
 );
