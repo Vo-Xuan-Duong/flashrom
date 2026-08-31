99817976466a1247e669e0484904b553e0ca555c
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-31 09:45:44 +0700

    feat: mount Platform Tools diagnostics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import PlatformToolsPanel from "./components/PlatformToolsPanel";
 import RecoveryCenter from "./components/RecoveryCenter";
 import RomArchivePanel from "./components/RomArchivePanel";
 import "./styles.css";
@@ -13,12 +14,14 @@ import "./guard.css";
 import "./restore.css";
 import "./restore-profile.css";
 import "./executor.css";
-import "./recovery-center.css";
+import "./platform-tools.css";
 import "./archive-panel.css";
+import "./recovery-center.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <PlatformToolsPanel />
     <RomArchivePanel />
     <RecoveryCenter />
   </StrictMode>,
