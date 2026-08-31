0cf5763b9ad1b6082ee443f04b03f218bab96386
Võ Xuân Dương <xuanduong.261104@gmail.com>
2026-08-31 09:40:00 +0700

    feat: mount Recovery Center

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import RecoveryCenter from "./components/RecoveryCenter";
 import "./styles.css";
 import "./rom.css";
 import "./plan.css";
@@ -11,9 +12,11 @@ import "./guard.css";
 import "./restore.css";
 import "./restore-profile.css";
 import "./executor.css";
+import "./recovery-center.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <RecoveryCenter />
   </StrictMode>,
 );
